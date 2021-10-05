17388debc35a9ab4cf300f7ca4ce85f387b76653
Priyanshi Nagpal <priyanshinagpal16@gmail.com>
2021-10-06 00:02:47 +0530

    Create 561. Array Partition I.java

---
+++ Leetcode/Array/561. Array Partition I.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maximized_sum=0;
+        int n=nums.length;
+        for(int i=0; i<n; i+=2){
+            maximized_sum += nums[i];
+        }
+        return maximized_sum;
+    }
+}
