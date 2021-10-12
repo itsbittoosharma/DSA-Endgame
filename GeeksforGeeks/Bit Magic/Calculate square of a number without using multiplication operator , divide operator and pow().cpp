69739fb9f99eaf7e37e3e99bcdc562405f4da058
Aashim12 <56119380+Aashim12@users.noreply.github.com>
2021-10-12 07:58:10 +0530

    Update Calculate square of a number without using multiplication operator , divide operator and pow().cpp

--- GeeksforGeeks/Bit Magic/Calculate square of a number without using multiplication operator , divide operator and pow().cpp
+++ GeeksforGeeks/Bit Magic/Calculate square of a number without using multiplication operator , divide operator and pow().cpp
@@ -1,4 +1,6 @@
-// Square of a number using bitwise operators
+// Problem statement : Calculate square of a number without using *, / and pow()
+// Link to the problem : https://www.geeksforgeeks.org/calculate-square-of-a-number-without-using-and-pow/#:~:text=Given%20an%20integer%20n%2C%20calculate,*%2C%20%2F%20and%20pow().&text=A%20Simple%20Solution%20is%20to%20repeatedly%20add%20n%20to%20result.
+
 #include <bits/stdc++.h>
 using namespace std;
  
