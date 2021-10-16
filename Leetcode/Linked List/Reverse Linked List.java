df9a170a0bc161455975993eb766bde90b6e1243
Karthiks08 <73209015+Karthiks08@users.noreply.github.com>
2021-10-16 08:22:46 +0530

    Reverse Linked List.java

---
+++ Leetcode/Linked List/Reverse Linked List.java
@@ -0,0 +1,47 @@
+
+Viewed
+@@ -0,0 +1,36 @@
+Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+
+
+// Time Complexity: O(n), Linear - traverse linked list only once
+// Space Complexity: O(1), Constant - we will only have 2 pointers regardless of size of input; prev and temp
+public ListNode reverseList(ListNode head) {
+    /* iterative solution */
+    ListNode newHead = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
+
+public ListNode reverseList(ListNode head) {
+    /* recursive solution */
+    return reverseListInt(head, null);
+}
+
+private ListNode reverseListInt(ListNode head, ListNode newHead) {
+    if (head == null)
+        return newHead;
+    ListNode next = head.next;
+    head.next = newHead;
+    return reverseListInt(next, head);
+}
