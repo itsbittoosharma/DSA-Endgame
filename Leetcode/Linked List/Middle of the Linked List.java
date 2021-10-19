0123d52492f60f7e97a43dd0416ccdea5c0e7410
Karthiks08 <73209015+Karthiks08@users.noreply.github.com>
2021-10-19 11:31:11 +0530

    updated the question,constarints and link

--- Leetcode/Linked List/Middle of the Linked List.java
+++ Leetcode/Linked List/Middle of the Linked List.java
@@ -1,4 +1,8 @@
- Middle of the Linked List
+Question Link: https://leetcode.com/problems/middle-of-the-linked-list/
+
+876. Middle of the Linked List
+
+Problem Description:
 
 Given the head of a singly linked list, return the middle node of the linked list.
 
@@ -19,6 +23,12 @@ Input: head = [1,2,3,4,5,6]
 Output: [4,5,6]
 Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
   
+ 
+ Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+
   
   public ListNode middleNode(ListNode head) {
     ListNode curr = head;
