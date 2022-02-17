312e01a1d331c375b54c6a8b74074e5a146c3564
Utkarsh Upadhyay <utkarshup562@gmail.com>
2022-02-17 10:00:53 +0530

    Time: 3 ms (89.64%), Space: 44.8 MB (24.49%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,28 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans1=new ArrayList<List<Integer>>();
-        ArrayList<Integer> ans=new ArrayList<Integer>();
-        Arrays.sort(candidates);
-        recursion(candidates, 0, target, new ArrayList<>(), ans1);
-        return ans1;
+        List<List<Integer>> list=new ArrayList<List<Integer>>();
+        helper(candidates, 0, target, list, new ArrayList<>());
+        return list;
     }
-    public void recursion(int[] nums, int index, int target, ArrayList<Integer> ans, List<List<Integer>> ans1)
+    public void helper(int[] arr, int ind, int target, List<List<Integer>> list, ArrayList<Integer> temp)
     {
-        if(index==nums.length)
+        if(target==0) {list.add(new ArrayList<>(temp)); return;}
+        if(ind==arr.length) return;
+        helper(arr, ind+1, target, list, temp);
+        if(arr[ind]<=target) 
         {
-            if(target==0) {ans1.add(new ArrayList<>(ans)); return;}
-         return;
-        }    
-        if(target==0)
-        {
-            ans1.add(new ArrayList<>(ans)); return;
+            temp.add(arr[ind]);
+            helper(arr, ind, target-arr[ind],list,temp);
+            temp.remove(temp.size()-1);
         }
-        if(nums[index]<=target)
-        {
-            ans.add(nums[index]);
-            recursion(nums, index,target-nums[index],ans,ans1);
-            ans.remove(ans.size()-1);
-            recursion(nums,index+=1,target,ans,ans1);
+        
     }
-}
 }
