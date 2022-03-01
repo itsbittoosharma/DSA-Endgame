f045934d7fa7005a0dc315c23c2bf558ff93e3c3
Utkarsh Upadhyay <utkarshup562@gmail.com>
2022-03-01 18:59:12 +0530

    Added solution - LeetHub

---
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+}
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            pair product = obj.getMinMax(a, n); 
+            System.out.println(product.first+" "+product.second);
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+ class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+        long max=Integer.MIN_VALUE;
+        long min=Integer.MAX_VALUE;
+        for(long i:a)
+        {
+            if(i<min) min=i;
+            if(i>max) max=i;
+        }
+        return new pair(min,max);
+    }
+}
