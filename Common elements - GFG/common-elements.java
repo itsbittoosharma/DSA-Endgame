568598bd3c56b737a83d5b72ed2b2f8b26a1ddbf
Utkarsh Upadhyay <utkarshup562@gmail.com>
2022-04-20 10:01:54 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.java
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        ArrayList<Integer> ans=new ArrayList<>();
+        int i=0,j=0,k=0;
+        int last=Integer.MAX_VALUE;
+        for(;i<n1&&j<n2&&k<n3;)
+        {
+            if(A[i]==B[j]&& B[j]==C[k]) {if(last!=A[i]) ans.add(A[i]); last=A[i]; i++; j++; k++;}
+            else if(A[i]<B[j]) i++;
+            else if(B[j]<C[k]) j++;
+            else k++;
+        }
+        return ans;
+    }
+}
