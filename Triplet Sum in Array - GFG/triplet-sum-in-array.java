c0254303489912ab7a725cd5403962dcedba2ead
Utkarsh Upadhyay <utkarshup562@gmail.com>
2022-04-28 21:31:59 +0530

    Added solution - LeetHub

---
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.java
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int X = Integer.parseInt(inputLine[1]);
+		    int A[] = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        A[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    Solution ob=new Solution();
+		    boolean ans = ob.find3Numbers(A, n, X);
+		    System.out.println(ans?1:0);
+		}
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    public static boolean find3Numbers(int A[], int n, int X) { 
+    Arrays.sort(A);
+    for(int i=0;i<A.length-2&&A[i]<X;i++)
+    {
+        int j=i+1, k=A.length-1;
+        while(j<k)
+        {
+            if(A[i]+A[j]+A[k]==X) return true;
+            else if(A[i]+A[j]+A[k]<X) j++;
+            else k--;
+        }
+    }
+    return false;
+    }
+}
