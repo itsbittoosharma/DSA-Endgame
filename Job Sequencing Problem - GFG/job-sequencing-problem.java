bb571f78a9fcd3565bc81ab163d2f89827fc5aac
Utkarsh Upadhyay <utkarshup562@gmail.com>
2022-07-14 19:48:29 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.java
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -43,53 +43,35 @@ class GfG {
 
 class Solution
 {
-    boolean checkavail(int deadline,int dead[])
-    {
-        deadline-=1;
-        if(dead[deadline]==0)
-        {
-            dead[deadline]=1;
-            return true;
-        }
-        else{
-            for(int i=deadline-1;i>=0;i--)
-            {
-                if(dead[i]==0)
-                {
-                    dead[i]=1;
-                    return true;
-                }
-            }
-            
-        }
-        return false;
-    }
     //Function to find the maximum profit and the number of jobs done.
     int[] JobScheduling(Job arr[], int n)
     {
-        if(arr==null) return new int[]{0,0};
-        if(arr.length==1) return new int[]{1,arr[0].profit};
-        int max=0;
-        int maxdead=0;
-        for(Job i:arr) maxdead=Math.max(maxdead,i.deadline);
-        Arrays.sort(arr,(a, b) -> b.profit - a.profit);
-        if(maxdead==1) return new int[]{1,arr[0].profit};
+        HashSet<Integer> hs=new HashSet<>();
+        Arrays.sort(arr,(a,b)->b.profit-a.profit );
         int profit=0;
-        int[] dead=new int[maxdead];
-        int c=0;
-        for(int i=0;i<maxdead;i++) dead[i]=0;
-        for(Job i:arr)
+        for(int i=0;i<n;i++)
         {
-            if(checkavail(i.deadline,dead))
+            if(!hs.contains(arr[i].deadline)) 
             {
-                profit+=i.profit;
-                c+=1;
+                hs.add(arr[i].deadline);
+                profit+=arr[i].profit;
+            }
+            else
+            {
+                int tempDeadline=arr[i].deadline;
+                while(tempDeadline!=0)
+                {
+                    if(!hs.contains(tempDeadline)) break;
+                    tempDeadline--;
+                }
+                if(tempDeadline>0) 
+                {
+                    hs.add(tempDeadline);
+                    profit+=arr[i].profit;
+                }
             }
         }
-        
-        
-        
-        return new int[]{c,profit};
+        return new int[]{hs.size(),profit};
     }
 }
 
